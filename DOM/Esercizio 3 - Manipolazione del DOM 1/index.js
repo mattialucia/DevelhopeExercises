58a9387e9b1436d2ad38146d4831f3abdf2c5959
Mattia Lucia <mattia.lucia.00@gmail.com>
2025-03-20 09:58:22 +0100

    fix: DOM - Modifiche es3 e es4

--- DOM/Esercizio 3 - Manipolazione del DOM 1/index.js
+++ DOM/Esercizio 3 - Manipolazione del DOM 1/index.js
@@ -1,21 +1,22 @@
-function addProduct() {
-  const input = document.querySelector("input"); // Seleziona la input field
-  const taskText = input.value; // Prende il valore digitato nella input field
+function addProduct(event) {
+  event.preventDefault() // Metodo per prevenire il comportamento di default del button
+  const input = document.querySelector("input") // Seleziona la input field
+  const taskText = input.value // Prende il valore digitato nella input field
   if (taskText === "") {
-    alert("Non lasciare il campo vuoto!"); // Alert se la input field rimane vuota
+    alert("Non lasciare il campo vuoto!") // Alert se la input field rimane vuota
     console.log("Errore: Input field vuota.")
     return
   }
-  const ul = document.querySelector("ul"); // Seleziona l'elemento <ul>
-  const li = document.createElement("li"); // Crea l'elemento <li>
+  const ul = document.querySelector("ul") // Seleziona l'elemento <ul>
+  const li = document.createElement("li") // Crea l'elemento <li>
 
-  const checkbox = document.createElement("input"); // Crea la un nuovo elemento di input
-  checkbox.type = "checkbox"; // Di tipo checkbox
+  const checkbox = document.createElement("input") // Crea la un nuovo elemento di input
+  checkbox.setAttribute("type", "checkbox") // Di tipo checkbox checkbox.type = "checkbox"
 
-  li.appendChild(checkbox); // Aggiunge la checkbox a <li>
-  li.appendChild(document.createTextNode(taskText)); // Aggiunge il testo alla <li>
-  ul.appendChild(li); // Aggiunge la <li> alla <ul>
-
+  li.appendChild(checkbox) // Aggiunge la checkbox a <li>
+  li.appendChild(document.createTextNode(taskText)) // Aggiunge il testo alla <li>
+  ul.appendChild(li) // Aggiunge la <li> alla <ul>
+  
   console.log(`Task "${taskText}" aggiunta correttamente.`)
-  input.value = ""; // Pulisce l'area testuale
+  input.value = "" // Pulisce l'area testuale
 }
--- DOM/Esercizio 4 - Manipolazione del DOM 2/index.js
+++ DOM/Esercizio 4 - Manipolazione del DOM 2/index.js
@@ -1,24 +1,28 @@
-function createPersonObject() {
-  setTimeout(() => {
-    // Recupero i valori di ciascun campo di input
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const age = document.getElementById("age").value;
+function createPersonObject(event) {
+  event.preventDefault() // Metodo per prevenire il comportamento di default
+  // Recupero i valori di ciascun campo di input
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const age = document.getElementById("age").value;
 
-    // Creo l'oggetto person con i valori recuperati in input
-    const person = { firstName: firstName, lastName: lastName, age: age };
-    console.log("Valori recuperati correttamente, ecco l'oggetto person:\n", person);
+  // Creo l'oggetto person con i valori recuperati in input
+  const person = { firstName: firstName, lastName: lastName, age: age };
+  console.log(
+    "Valori recuperati correttamente, ecco l'oggetto person:\n",
+    person
+  );
 
-    // Converto l'oggetto in formato JSON
-    const personJSON = JSON.stringify(person);
-    console.log("Converto l'oggetto in formato JSON:\n", personJSON)
+  // Converto l'oggetto in formato JSON
+  const personJSON = JSON.stringify(person);
+  console.log("Converto l'oggetto in formato JSON:\n", personJSON);
 
-    // Recupero il form
-    const form = document.querySelector("form");
+  // Recupero il form
+  const form = document.querySelector("form");
 
-    // Aggiungo l'attributo data-person al JSON
-    form.setAttribute("data-person", personJSON);
-  }, 3000);
+  // Aggiungo l'attributo data-person al JSON
+  form.setAttribute("data-person", personJSON);
+  console.log("Stampo il form con il nuovo attributo data-person:\n", form)
 }
 
-createPersonObject();
+const form = document.querySelector("form");
+form.addEventListener("submit", createPersonObject);
